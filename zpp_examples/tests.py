058fb9d75912c30e263aa6a23f5913b874bb794d
kuba <kuba.pierewoj@gmail.com>
2017-05-13 10:03:37 +0100

    testy na gradienty c++

--- zpp_examples/tests.py
+++ zpp_examples/tests.py
@@ -1,10 +1,19 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import numpy as np
+from tensorflow.python.framework import ops
+import tensorflow as tf
+from tensorflow.python.framework import dtypes
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import gen_nn_ops
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import nn_ops
+from tensorflow.python.ops import sparse_ops
 
-import argparse
-import sys
-import time
 
 from tensorflow.examples.tutorials.mnist import input_data
 from tensorflow.python.framework import function
@@ -14,6 +23,21 @@ from tensorflow.python.ops import sparse_ops
 import unittest
 import tensorflow as tf
 import numpy as np
+
+@ops.RegisterGradient("BinaryConv2D")
+def _Conv2DGrad(op, grad):
+  return [nn_ops.conv2d_backprop_input(
+      array_ops.shape(op.inputs[0]), op.inputs[1], grad, op.get_attr("strides"),
+      op.get_attr("padding"), op.get_attr("use_cudnn_on_gpu"),
+      op.get_attr("data_format")),
+          nn_ops.conv2d_backprop_filter(op.inputs[0],
+                                        array_ops.shape(op.inputs[1]), grad,
+                                        op.get_attr("strides"),
+                                        op.get_attr("padding"),
+                                        op.get_attr("use_cudnn_on_gpu"),
+                                        op.get_attr("data_format"))]
+
+
 
 def conv2d(x, W, padding):
   return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding=padding)
@@ -51,7 +75,47 @@ def compute_ff_cpp(input_shape, x_shape, w, input, padding):
   result_bin = h_conv_bin.eval(session=sess, feed_dict={x: input})
   return result_bin
 
+def compute_grad_python(input_shape, x_shape,w, input, padding):
+  x = tf.placeholder(tf.float32, input_shape)
+  x_reshaped = tf.reshape(x, x_shape)
+  W_conv1 = tf.Variable(tf.cast(w,dtype=tf.float32))
+  h_conv_bin = conv_bin(x_reshaped, W_conv1, padding)
+  grad = tf.gradients(h_conv_bin, W_conv1)  
+  sess = tf.Session()
+  sess.as_default()
+  sess.run(tf.global_variables_initializer())
+  grad_evaled = grad[0].eval(session=sess, feed_dict={x: input})
+  return grad_evaled 
+
+def compute_grad_cpp(input_shape, x_shape,w, input, padding):
+  jit_scope = tf.contrib.compiler.jit.experimental_jit_scope
+  with jit_scope():
+    x = tf.placeholder(tf.float32, input_shape)
+    x_reshaped = tf.reshape(x, x_shape)
+    W_conv1 = tf.Variable(tf.cast(w,dtype=tf.float32))
+    conv_bin_module = tf.load_op_library('../tensorflow/core/user_ops/conv_bin.so')
+    h_conv_bin = conv_bin_module.binary_conv2d(x_reshaped, W_conv1, strides=[1,1,1,1], padding=padding)
+    grad = tf.gradients(h_conv_bin, W_conv1)  
+    sess = tf.Session()
+    sess.as_default()
+    sess.run(tf.global_variables_initializer())
+    grad_evaled = grad[0].eval(session=sess, feed_dict={x: input})
+    return grad_evaled 
+
 class TestStringMethods(unittest.TestCase):
+  def test_grad_simple(self):
+    x_shape = [-1,3,3,1]
+    input_shape = [None,9]
+    w = np.zeros([2,2,1,1])
+    w[0:2,0:2,0,0] = np.vstack(([1,1],[1,1]))
+    padding = 'VALID'
+    input = np.reshape(np.array([1,2,3,4,5,6,7,8,9], dtype=np.float32),(1,9))
+    cpp = compute_grad_cpp(input_shape, x_shape, w, input, padding)   
+    python = compute_grad_python(input_shape,x_shape,w,input, padding)    
+    print("cpp",cpp)
+    print("python",python)
+    self.assertTrue(np.array_equal(cpp, python))
+
   def test_simple(self):
     x_shape = [-1,3,3,1]
     input_shape = [None,9]
@@ -62,7 +126,7 @@ class TestStringMethods(unittest.TestCase):
     cpp = compute_ff_cpp(input_shape, x_shape, w, input, padding)   
     python = compute_ff_python(input_shape,x_shape,w,input, padding)    
     self.assertTrue(np.array_equal(cpp, python))
-
+    
   def test_same_padding(self):
     x_shape = [-1,3,3,1]
     input_shape = [None,9]
@@ -71,18 +135,6 @@ class TestStringMethods(unittest.TestCase):
     padding = 'SAME'
     input = np.reshape(np.array([1,2,3,4,5,6,7,8,9], dtype=np.float32),(1,9))
     cpp = compute_ff_cpp(input_shape, x_shape, w, input, padding)   
-    python = compute_ff_python(input_shape,x_shape,w,input, padding)    
-    self.assertTrue(np.array_equal(cpp, python))
-
-  def test_multiple_inputs(self):
-    x_shape = [-1,3,3,1]
-    input_shape = [None,9]
-    w = np.zeros([2,2,1,1])
-    w[0:2,0:2,0,0] = np.vstack(([1,1],[1,1]))
-    numbers = np.reshape(np.array([1,2,3,4,5,6,7,8,9], dtype=np.float32),(1,9))
-    input = np.vstack((numbers,numbers))    
-    padding = 'VALID'
-    cpp = compute_ff_cpp(input_shape, x_shape, w, input,padding)   
     python = compute_ff_python(input_shape,x_shape,w,input, padding)    
     self.assertTrue(np.array_equal(cpp, python))
 
@@ -95,9 +147,50 @@ class TestStringMethods(unittest.TestCase):
     input = np.reshape(np.array([1,2,3,4,5,6,7,8,9], dtype=np.float32),(1,9))
     cpp = compute_ff_cpp(input_shape, x_shape, w, input, padding)   
     python = compute_ff_python(input_shape,x_shape,w,input, padding)    
-    print('python',python)
-    print('cpp', cpp)
     self.assertTrue(np.array_equal(cpp, python))
 
+  def test_negative_weights_multibatch(self):
+    x_shape = [-1,3,3,1]
+    input_shape = [None,9]
+    w = np.zeros([2,2,1,1])
+    w[0:2,0:2,0,0] = np.vstack(([1,-2],[1,1]))
+    padding = 'VALID'
+    input = np.reshape(np.array([1,2,3,4,5,6,7,8,9,1,2,3,20,11,33,-5,1,9], dtype=np.float32),(2,9))
+    cpp = compute_ff_cpp(input_shape, x_shape, w, input, padding)   
+    python = compute_ff_python(input_shape,x_shape,w,input, padding)    
+    self.assertTrue(np.array_equal(cpp, python))
+
+  def test_grad_negative_weights_when_same_padding(self):
+    x_shape = [-1,3,3,1]
+    input_shape = [None,9]
+    w = np.zeros([2,2,1,1])
+    w[0:2,0:2,0,0] = np.vstack(([1,-2],[1,1]))
+    padding = 'SAME'
+    input = np.reshape(np.array([1,2,3,4,5,6,7,8,9,1,2,3,20,11,33,-5,1,9], dtype=np.float32),(2,9))
+    cpp = compute_grad_cpp(input_shape, x_shape, w, input, padding)   
+    python = compute_grad_python(input_shape,x_shape,w,input, padding)    
+    self.assertTrue(np.array_equal(cpp, python))
+
+  def test_grad_negative_weights(self):
+    x_shape = [-1,3,3,1]
+    input_shape = [None,9]
+    w = np.zeros([2,2,1,1])
+    w[0:2,0:2,0,0] = np.vstack(([1,-2],[1,1]))
+    padding = 'VALID'
+    input = np.reshape(np.array([1,2,3,4,5,6,7,8,9,1,2,3,20,11,33,-5,1,9], dtype=np.float32),(2,9))
+    cpp = compute_grad_cpp(input_shape, x_shape, w, input, padding)   
+    python = compute_grad_python(input_shape,x_shape,w,input, padding)    
+    self.assertTrue(np.array_equal(cpp, python))
+  
+  def test_grad_negative_weights_multibatch(self):
+    x_shape = [-1,3,3,1]
+    input_shape = [None,9]
+    w = np.zeros([2,2,1,1])
+    w[0:2,0:2,0,0] = np.vstack(([1,-2],[1,1]))
+    padding = 'VALID'
+    input = np.reshape(np.array([1,2,3,4,5,6,7,8,9,1,2,3,20,11,33,-5,1,9], dtype=np.float32),(2,9))
+    cpp = compute_grad_cpp(input_shape, x_shape, w, input, padding)   
+    python = compute_grad_python(input_shape,x_shape,w,input, padding)    
+    self.assertTrue(np.array_equal(cpp, python))
 if __name__ == '__main__':
   unittest.main()
