0898047ebbf0c622b7a8bbfcbcdd5c5e5f897df5
kuba <kuba.pierewoj@gmail.com>
2017-06-04 20:35:08 +0100

    impl c++, dynamik, segfaulty leca

--- zpp_examples/run_python_valgrind.sh
+++ zpp_examples/run_python_valgrind.sh
@@ -1,2 +1,2 @@
-valgrind --tool=memcheck --suppressions=../tensorflow/tensorflow/core/user_ops/valgrind-python.supp python3.5 -E -tt ./example_script.py
+valgrind --tool=memcheck --suppressions=../tensorflow/core/user_ops/valgrind-python.supp python3.5 -E -tt ./example_script.py
 
